8a213810597fdfd2d689f9ef8d9dbc74d1c7e1c4
tBawa88 <tbawa3388@gmail.com>
2024-07-16 02:03:27 +0530

    fix: another attempt at fixing suspense boundary error

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -36,7 +36,9 @@ export default async function Page({
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
+                <Suspense fallback={<div>Loading.. </div>}>
+                    <Pagination totalPages={totalPages} />
+                </Suspense>
             </div>
         </div>
     );
