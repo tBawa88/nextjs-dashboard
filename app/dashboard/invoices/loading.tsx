965536d2697a8ce012a28781509764ea3d2c7317
tBawa88 <tbawa3388@gmail.com>
2024-07-16 02:13:52 +0530

    this one is definitely fixed, TRUST

--- app/dashboard/invoices/loading.tsx
+++ app/dashboard/invoices/loading.tsx
@@ -7,12 +7,8 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 const loading = () => {
     return (
         <div className="w-full">
-            <div className="flex w-full items-center justify-between">
+            <div className="flex w-full items-center justify-between mb-20">
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
-                <CreateInvoice />
             </div>
             <InvoicesTableSkeleton />
         </div>
